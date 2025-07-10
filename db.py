e651f1c08f6634de9953e7e372bf8a4438f166ac
Sabba Perween <sabbaperween14@gmail.com>
2025-07-10 16:37:31 +0530

    FIX: Remove load_dotenv to use Streamlit's native secrets

--- db.py
+++ db.py
@@ -23,11 +23,11 @@ def get_connection():
     """Establishes a connection to the database using st.secrets."""
     try:
         return psycopg2.connect(
-            dbname=os.getenv["DB_NAME"],
-            user=os.getenv["DB_USER"],
-            password=os.getenv["DB_PASSWORD"],
-            host=os.getenv["DB_HOST"],
-            port=os.getenv["DB_PORT"],
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
             sslmode='require' 
             )
     except psycopg2.OperationalError as e:
